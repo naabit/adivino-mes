5ea6cadefcb0a9eacdb2fbd13623b3bcae4807fc
naabit <nmt.garcia.c@gmail.com>
2025-07-07 17:32:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def resultado():
 
     return render_template("resultado.html", mensaje=mensaje)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
